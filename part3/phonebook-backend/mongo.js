03f68045ec46a2e02c7049cbc444e598c695d49d
Parvez Wijaya <parvezwijaya@gmail.com>
2025-06-19 14:28:55 +0700

    mongo test

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
-  console.log('Usage: node mongo.js <password> <name> <number>')
-  process.exit(1)
-}
+// if (process.argv.length < 5) {
+//   console.log('Usage: node mongo.js <password> <name> <number>')
+//   process.exit(1)
+// }
 
 console.log("argv length: " + process.argv.length)
 const password = process.argv[2]
 
-const url = `mongodb+srv://parvezwijaya:NnE10uQWITWUIRfO@cluster0.zglhvth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://parvezwijaya:${password}@cluster0.zglhvth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -42,8 +42,8 @@ const name = process.argv[3]
 const number = process.argv[4]
 
 const person = new Person({
-    name: name,
-    number: number,
+  name: name,
+  number: number,
 })
 console.log(person)
 person.save().then(res => {
